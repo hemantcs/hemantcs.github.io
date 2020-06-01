11f8f06f60c32a58502c5e6119d318a50b4b1e2c
Hemant <hemantksingh246@gmail.com>
2020-05-31 22:11:29 -0700

    Updates

---
+++ precache-manifest.7cfc6a4d74f5dd7996cc3486bffb7997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b960e44a61acc515c235e3cbd9fd0a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07edac3e035a1a1ba1ac",
+    "url": "/static/css/main.578fe9c1.chunk.css"
+  },
+  {
+    "revision": "c6aeb7089b7f93a7c012",
+    "url": "/static/js/2.b71f169f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b71f169f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07edac3e035a1a1ba1ac",
+    "url": "/static/js/main.59c0457c.chunk.js"
+  },
+  {
+    "revision": "37cee4e6e9cc10b2a308",
+    "url": "/static/js/runtime-main.29637e6a.js"
+  },
+  {
+    "revision": "d20956516e72ca866dcaa1a938864749",
+    "url": "/static/media/hemant_Resume.d2095651.pdf"
+  }
+]);
